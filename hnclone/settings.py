d538dafabab8804ffe825fc30b66994f81d07289
Simone Romano <me@simoneromano.com>
2020-11-05 11:39:51 +0200

    Make it work

--- hnclone/settings.py
+++ hnclone/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'TODO' # TODO
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 USE_X_FORWARDED_HOST = True
@@ -141,10 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "assets"),
 ]
-
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -166,9 +166,12 @@ LOGIN_REDIRECT_URL = '/'
 
 
 
-ACCEPT_UNINVITED_REGISTRATIONS = False
+ACCEPT_UNINVITED_REGISTRATIONS = True
 
 
 SITE_NAME = 'Pythonic News'
 SITE_URL = 'https://news.python.sc'
 SITE_DOMAIN = 'news.python.sc'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
